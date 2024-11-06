c96dc552fdf9b3007e6897714756c7a705039367
renatof4rias <renatof4rias@gmail.com>
2024-11-06 16:19:19 -0300

    feat: Equals pt1

---
+++ src/java/colecoes.test/ColecaoTest01.java
@@ -0,0 +1,14 @@
+package colecoes.test;
+
+public class ColecaoTest01 {
+    public static void main(String[] args) {
+
+        Carro c1 = new Carro("FIAT UNO", "2013");
+        Carro c2 = new Carro("FIAT UNO", "2013");
+
+        System.out.println(c1.equals(c2));
+
+
+    }
+
+}
