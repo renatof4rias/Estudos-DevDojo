62fc019c56d9e3781c1f5126f78b2a247354fd59
renatof4rias <renatof4rias@gmail.com>
2024-11-07 16:30:30 -0300

    feat: List pt 1

--- src/java/colecoes.test/ListTest01.java
+++ src/java/colecoes.test/ListTest01.java
@@ -6,23 +6,23 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
 
-
         // Expectate type Object, with inconsistency
         List tipoObject = new ArrayList();
         tipoObject.add("ajsdhfakjsdhf");
         tipoObject.add(1234);
-
-
 
         // Expectate Only String, with consistency
         List<String> tipoGenerics = new ArrayList<>();
         tipoGenerics.add("asdfasdf");
         tipoGenerics.add("asdfasnfjkas");
 
+        for (int i = 0; i < tipoObject.size(); i++) {
+            System.out.println(tipoObject.get(i));
+        }
 
-
-
-
+        for (String tipoGeneric: tipoGenerics) {
+            System.out.println(tipoGeneric);
+        }
 
 
     }
